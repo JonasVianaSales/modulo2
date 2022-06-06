a267bef95bd3899e302de6748d7c08ddaa9b228d
JonasVianaSales <jonas.sales@sou.inteli.edu.br>
2022-06-06 20:53:12 -0300

    Semana 6

---
+++ 03_AUT_EST_ENTREGA/Semana 6/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const database = new sqlite3.Database('sqlite.db');
+const port = 3000;
+const hostname = '127.0.0.1';
+const DBPATH = 'sqlite.db';
+app.use(express.json())
+app.use(express.static("front/"));
+
+app.get('/uau', (req, res) =>{
+    res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    sql = 'SELECT * FROM skills ORDER BY id COLLATE NOCASE';
+    db.all(sql, [],  (err, rows) => {
+		if (err) {
+		    throw err;
+		}
+        console.log(rows);
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Database server running at http://${hostname}:${port}/`);
+  });
